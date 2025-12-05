8820685c86ba35e828ee8633a4551af6f9a12f7d
Jens Papenhagen <jens.papenhagen@web.de>
2025-12-05 18:41:11 +0100

    add spring boot 4 foundation

---
+++ src/main/java/de/papenhagen/chefkoch_scraper/ChefkochScraperApplication.java
@@ -0,0 +1,13 @@
+package de.papenhagen.chefkoch_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefkochScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefkochScraperApplication.class, args);
+	}
+
+}
