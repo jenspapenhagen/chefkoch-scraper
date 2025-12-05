eae98406fad565d71c6b337a7eeee8e773fd369f
Jens Papenhagen <jens.papenhagen@web.de>
2025-12-05 18:45:15 +0100

    adding dockerfile

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:17-jdk-alpine
+
+WORKDIR /app
+
+COPY target/chefkoch-scraper-0.0.1-SNAPSHOT.jar /app/app.jar
+
+EXPOSE 8080
+CMD ["java", "-jar", "/app/app.jar"]
--- src/main/java/de/papenhagen/chefkoch_scraper/ChefkochScraperApplication.java
+++ src/main/java/de/papenhagen/chefkoch_scraper/ChefkochScraperApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChefkochScraperApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChefkochScraperApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChefkochScraperApplication.class, args);
+    }
 
 }
