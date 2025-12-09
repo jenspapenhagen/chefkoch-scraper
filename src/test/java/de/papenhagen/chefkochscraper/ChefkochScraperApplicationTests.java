3b4dd52bbc950261f470edbde7016bd9b443076b
Jens Papenhagen <jens.papenhagen@web.de>
2025-12-09 18:53:32 +0100

    rename Test

--- src/test/java/de/papenhagen/chefkochscraper/ChefkochScraperApplicationTests.java
+++ src/test/java/de/papenhagen/chefkochscraper/ChefkochScraperApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChefkochScraperApplicationTests {
+class ChefkochScraperApplicationTest {
 
 	@Test
 	void contextLoads() {
